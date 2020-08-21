9de00f58a9aa6630cdc01a71c06db0dd83d52e97
habanero <haba.atsushi@donuts.ne.jp>
2020-08-21 17:48:32 +0900

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,78 @@
-var gulp = require('gulp');
-var merge = require('merge-stream');
-var del = require('del');
-var serve = require('gulp-serve');
-var rename = require('gulp-rename');
-var inlinesource = require('gulp-inline-source');
-var stylus = require('gulp-stylus');
+var gulp = require("gulp");
+var merge = require("merge-stream");
+var del = require("del");
+var serve = require("gulp-serve");
+var rename = require("gulp-rename");
+var inlinesource = require("gulp-inline-source");
+var stylus = require("gulp-stylus");
 
-var src = 'src/**/';
+var src = "src/**/";
 var dependencies = [
   // '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js',
   // 'node_modules/contentful-ui-extensions-sdk/dist/cf-extension.css',
   // 'node_modules/contentful-ui-extensions-sdk/dist/cf-extension-api.js',
 ];
 
-gulp.task('default', ['build'], function () {
-  gulp.start('serve');
+gulp.task("stylus", function () {
+  return gulp
+    .src(src + "*.styl")
+    .pipe(stylus())
+    .pipe(gulp.dest("./dist"));
 });
-
-// Serve and watch for changes so we don't have to run `gulp` after each change.
-gulp.task('watch', ['build'], function () {
-  gulp.start('serve');
-  gulp.watch([src + '*', dependencies], function () {
-    gulp.start(['build']);
-  });
-});
-
-// Serve dist folder on port 3000 for local development.
-gulp.task('serve', serve({
-  root: 'dist'
-}));
 
 // Copy required dependencies into dist folder.
-gulp.task('build', ['stylus'], function () {
+gulp.task(
+  "build",
+  gulp.series("stylus", function () {
+    var filesStream = gulp.src(src + "!(*.styl)").pipe(gulp.dest("./dist"));
 
-  var filesStream = gulp.src(src + '!(*.styl)')
-    .pipe(gulp.dest('./dist'));
+    // var depsStream = gulp
+    //   .src(dependencies, { allowEmpty: true })
+    //   .pipe(gulp.dest("./dist/lib"));
 
-  var depsStream = gulp.src(dependencies)
-    .pipe(gulp.dest('./dist/lib'));
+    // return merge(filesStream, depsStream);
+    return filesStream;
+  })
+);
 
-  return merge(filesStream, depsStream);
-});
+// Serve dist folder on port 3000 for local development.
+gulp.task(
+  "serve",
+  serve({
+    root: "dist",
+  })
+);
 
-gulp.task('stylus', function () {
-  return gulp.src(src + '*.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('./dist'));
-});
+// Serve and watch for changes so we don't have to run `gulp` after each change.
+gulp.task(
+  "watch",
+  gulp.series("build", function () {
+    gulp.start("serve");
+    gulp.watch([src + "*", dependencies], function () {
+      gulp.start(["build"]);
+    });
+  })
+);
 
 // Bundles the whole widget into one file which can be uploaded to Contentful.
-gulp.task('bundle', ['build'], function () {
-  return gulp.src('./dist/index.html')
-    .pipe(rename('index.min.html'))
-    .pipe(inlinesource())
-    .pipe(gulp.dest('./dist'));
+gulp.task(
+  "bundle",
+  gulp.series("build", function () {
+    return gulp
+      .src("./dist/index.html")
+      .pipe(rename("index.min.html"))
+      .pipe(inlinesource())
+      .pipe(gulp.dest("./dist"));
+  })
+);
+
+gulp.task("clean", function () {
+  return del(["./dist"]);
 });
 
-gulp.task('clean', function () {
-  return del([
-    './dist'
-  ]);
-});
+gulp.task(
+  "default",
+  gulp.series("build", function () {
+    gulp.start("serve");
+  })
+);
